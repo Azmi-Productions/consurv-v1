2ac88feec5bac953059fa76291ea9ddca089d17a
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-15 10:55:40 +0800

    fix: Typo on testimonials

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <main className="py-48 px-10 md:px-24 lg:px-40 bg-white">
       <header className="flex items-center justify-center gap-10 lg:px-40 mb-24">
-        <h1 className="text-zinc-800 text-5xl font-bold">Internships</h1>
+        <h1 className="text-zinc-800 text-5xl font-bold">Testimonials</h1>
       </header>
     </main>
   );
