0fae8a5f9db76b77a54e58d7c93b864390ffe908
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-29 11:10:14 +0800

    chore: Add more partners

--- app/whoweare/partners.ts
+++ app/whoweare/partners.ts
@@ -7,6 +7,17 @@ import LINDY from "@/public/whoweare/partners/logo1-1-150x98.jpg";
 import OPERAIO from "@/public/whoweare/partners/Hnet.com-image-7-150x57.png";
 import DATALOCKER from "@/public/whoweare/partners/Hnet.com-image-2-150x75.png";
 import LENOVO from "@/public/whoweare/partners/Hnet.com-image-4.png";
+import PETRONAS from "/public/whoweare/partners/Petronas_Logo.svg.png";
+import PETROFAC from "/public/whoweare/partners/petrofac-logo-0FE5906F0A-seeklogo.com.png";
+import SHELL from "/public/whoweare/partners/Shell_logo.svg.png";
+import EXXONMOBIL from "/public/whoweare/partners/Exxon_Mobil_Logo.svg.png";
+import HIBISCUS from "/public/whoweare/partners/Hibiscus-Petroleum_Master_Logo_transparent.png";
+import SD from "/public/whoweare/partners/sime-darby-logo-DA85D99D2A-seeklogo.com.png";
+import PTTEP from "/public/whoweare/partners/PTTEP_Logo.svg.png";
+import MICROSOFT from "/public/whoweare/partners/Microsoft_logo_(2012).svg.png";
+import FORTINET from "/public/whoweare/partners/Fortinet_logo.svg.png";
+import ORACLE from "/public/whoweare/partners/Oracle-Logo.png";
+import JUNIPER from "/public/whoweare/partners/Juniper_Networks_logo.svg.png";
 
 export const partners = [
   {
@@ -53,5 +64,60 @@ export const partners = [
     name: "Lenovo",
     image: LENOVO,
     link: "https://www.lenovo.com/my/en/",
+  },
+  {
+    name: "Petronas",
+    image: PETRONAS,
+    link: "https://www.petronas.com/",
+  },
+  {
+    name: "Petrofac",
+    image: PETROFAC,
+    link: "https://www.petrofac.com/",
+  },
+  {
+    name: "Shell",
+    image: SHELL,
+    link: "https://www.shell.com/",
+  },
+  {
+    name: "ExxonMobil",
+    image: EXXONMOBIL,
+    link: "https://corporate.exxonmobil.com/",
+  },
+  {
+    name: "Hibiscus",
+    image: HIBISCUS,
+    link: "https://www.hibiscuspetroleum.com/",
+  },
+  {
+    name: "Sime Darby",
+    image: SD,
+    link: "https://www.simedarby.com/",
+  },
+  {
+    name: "PTTEP",
+    image: PTTEP,
+    link: "https://www.pttep.com/",
+  },
+  {
+    name: "Microsoft",
+    image: MICROSOFT,
+    link: "https://www.microsoft.com/",
+  },
+  {
+    name: "Fortinet",
+    image: FORTINET,
+    link: "https://www.fortinet.com/",
+  },
+  {
+    name: "Oracle",
+    image: ORACLE,
+    link: "https://www.oracle.com/",
+  },
+  {
+    name: "Juniper",
+    image: JUNIPER,
+    link: "https://www.juniper.net/",
   },
 ];
